7f3142d1a6a55a5ae14efc4905aa91634f2f4589
Citizen-XX <thlorient@gmail.com>
2024-08-28 13:52:21 -0600

    Slight client.py modification

--- LlamaOpenAI/PracticeActivity/client.py
+++ LlamaOpenAI/PracticeActivity/client.py
@@ -2,11 +2,10 @@ import requests
 
 base_url = "http://127.0.0.1:8000"
 endpoint = "/query_data"
-data_1 = {"text": "Please tell me what Deep Learning is used for"}
-data_2 = {"text": "Please tell me what Unsupervised Learning is used for"}
-data_3 = {"text": "Please tell me what Unsupervised Learning's applications are"}
+query = input("What's you question about Machine Learning?: ")
+data = {"text": str(query)}
 
-response = requests.post(url=f"{base_url+endpoint}",json=data_3)
+response = requests.post(url=f"{base_url+endpoint}",json=data)
 
 if response.status_code == 200:
     print("Response: ",response.json()["result"]["response"])
